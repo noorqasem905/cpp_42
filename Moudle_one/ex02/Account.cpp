7c796da51b49198495588f1627862e643b64a283
noor aldeen qasem <noorqasem905@gmail.com>
2025-07-23 19:35:23 +0300

    norm

---
+++ Moudle_one/ex02/Account.cpp
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nqasem <nqasem@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 09:19:49 by nqasem            #+#    #+#             */
+/*   Updated: 2025/07/23 19:26:44 by nqasem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+// Account::Account
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount( void )
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t now = std::time(0);
+	struct tm *ltm = std::localtime(&now);
+	char			log_time[19];
+
+	std::strftime (log_time, 19, "[%G%m%d_%H%M%S] ", ltm);
+	std::cout << log_time;
+}
+
+Account::Account(int initial_deposit)
+	: _accountIndex(_nbAccounts++), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex
+			  << "; amount:" << _amount
+			  << "; created" << std::endl;
+}
+
+Account::~Account()
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex
+			  << "; amount:" << _amount	
+			  << "; closed" << std::endl;
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts()
+			  << "; total:" << getTotalAmount()
+			  << "; deposits:" << getNbDeposits()
+			  << "; withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	_amount += deposit;
+	_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	std::cout << " index:" << _accountIndex
+			  << "; p_amount:" << _amount - deposit
+			  << "; deposit:" << deposit
+			  << "; amount:" << _amount
+			  << "; nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_displayTimestamp();
+	if (withdrawal > _amount)
+	{
+		std::cout << " index:" << _accountIndex
+				  << "; p_amount:" << _amount
+				  << "; withdrawal:refused" << std::endl;
+		return false;
+	}
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	std::cout << " index:" << _accountIndex
+			  << "; p_amount:" << _amount + withdrawal
+			  << "; withdrawal:" << withdrawal
+			  << "; amount:" << _amount
+			  << "; nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return true;
+}
+int	Account::checkAmount() const
+{
+	return _amount;
+}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex
+			  << "; amount:" << _amount
+			  << "; deposits:" << _nbDeposits
+			  << "; withdrawals:" << _nbWithdrawals << std::endl;
+}
--- Moudle_one/ex02/tests.cpp
+++ Moudle_one/ex02/tests.cpp
@@ -9,6 +9,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <functional>
 #include "Account.hpp"
 
@@ -38,7 +39,7 @@ int		main( void ) {
 	ints_t::iterator	wit_end		= withdrawals.end();
 
 	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	std::for_each(acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
@@ -53,7 +54,6 @@ int		main( void ) {
 	for ( acc_int_t it( acc_begin, wit_begin );
 		  it.first != acc_end && it.second != wit_end;
 		  ++(it.first), ++(it.second) ) {
-
 		(*(it.first)).makeWithdrawal( *(it.second) );
 	}
 
@@ -62,7 +62,6 @@ int		main( void ) {
 
 	return 0;
 }
-
 
 // ************************************************************************** //
 // vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
