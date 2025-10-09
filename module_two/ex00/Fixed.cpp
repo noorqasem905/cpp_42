f2635529f3ac8a1094672b460cde50a5aa580df7
Noor aldeen <noorqasem905@gmail.com>
2025-10-09 17:52:23 +0300

    add

--- module_two/ex00/Fixed.cpp
+++ module_two/ex00/Fixed.cpp
@@ -6,7 +6,40 @@
 /*   By: nqasem <nqasem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:59:10 by nqasem            #+#    #+#             */
-/*   Updated: 2025/10/07 16:59:11 by nqasem           ###   ########.fr       */
+/*   Updated: 2025/10/09 17:48:26 by nqasem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+class Fixed
+{
+private:
+    int fixed_point;
+    static const int fractional_bits = 8;
+public:
+    
+    Fixed()
+    {
+        fixed_point = 0;
+    }
+    Fixed(Fixed const &other)
+    {
+        fixed_point = other.fixed_point;
+    }
+    int getRawBits( void ) const
+    {
+        return fixed_point;
+    }
+    void setRawBits( int const raw )
+    {
+        fixed_point = raw;  
+    }
+    ~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
